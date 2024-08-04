381db1316bb5ea9c8c9481e9c288ef28490b6bc1
Sebastian Novak <sebastiannovak145@gmail.com>
2024-08-05 00:49:43 +0200

    faster crash

--- zadatak/python-server/server.py
+++ zadatak/python-server/server.py
@@ -10,8 +10,7 @@ def health_check():
 
 @app.route('/crash', methods=['GET'])
 def crash():
-    os.kill(os.getpid(), signal.SIGINT)
-    return "Crashing the app...", 500
+    os._exit(1)  # Immediately terminates the process
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
