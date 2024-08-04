4f080bded94252cd91f1ade2d586cf0cc3d31804
Sebastian Novak <sebastiannovak145@gmail.com>
2024-08-05 00:41:28 +0200

    Update zadatak.sh

--- zadatak/zadatak.sh
+++ zadatak/zadatak.sh
@@ -1,5 +1,12 @@
 
 ## Zadatak ovde
+## Napisati skriptu koja ce proveravati da li je server ziv 
+## i ako nije, restartovati ga (prvo stop pa start)
+## DODATNO:
+## Skripta treba da se izvrsava perioedicno, svakih 1 minun kao cronjob
+## crontab -e 
+## Skipta takodje treba da vodi racuna o logovanju i da u slucaju greske
+## posalje email obavestenje na email adresu alerts-linux@rajak.rs
 
 source config.sh
 source logger.sh
